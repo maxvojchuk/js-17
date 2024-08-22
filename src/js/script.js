3357a666e6b4730e7de9ebee67d0f3174bd5d9c6
maxvojcuk <maksimvojcuk09@gmail.com>
2024-08-22 21:19:51 +0300

    дз

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+//1
+const input = document.querySelector("#input");
+const button = document.querySelector("#button");
+const changeButton = () => {
+  const input = textField.value;
+  button.textContent = input;
+};
+button.addEventListener("click", changeButton);
+//2
+const img = document.querySelector("#img");
+img.src = "/src/img/carrot.png";
+
+//3
+const image = document.querySelector("#photo");
+const link = document.querySelector("#link");
+link.href =
+  "https://uk.wikipedia.org/wiki/%D0%A0%D0%B0%D0%BA%D0%B5%D1%82%D0%B0";
+image.alt = "Ракета";
+//4
+const list = document.querySelector("#list");
+const firsItem = list.children[0];
+firsItem.textContent = "Текст один";
